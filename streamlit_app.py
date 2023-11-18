ffbd94350288462143c651a8c4a508eeeb7697a1
Akmalia Fatimah <101547800+akmalia3@users.noreply.github.com>
2023-11-18 22:30:53 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,33 +1,22 @@
-import pandas as pd
 import streamlit as st
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
 st.subheader('Wordcloud Generator')
-st.write('Enjoy the app 😊')
+st.write('Enter your text below and click the button to generate a Word Cloud 😊')
 
-uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
+# Text input box
+user_input = st.text_area("Enter your text here:", "")
 
-if uploaded_files:
-    for uploaded_file in uploaded_files:
-        df = pd.read_csv(uploaded_file)
-        st.write("Filename:", uploaded_file.name)
-        st.dataframe(df)
+# Generate Word Cloud
+if st.button('Generate Word Cloud'):
+    if user_input:
+        wordcloud = WordCloud().generate(user_input)
 
-        # Get the first text column for Word Cloud generation
-        text_columns = [col for col in df.columns if df[col].dtype == 'object']
-        if not text_columns:
-            st.warning("No text columns found in the CSV file.")
-        else:
-            selected_column = st.selectbox("Select a text column for Word Cloud", text_columns)
-            text_data = ' '.join(df[selected_column])
-
-            # Generate Word Cloud
-            if st.button('Generate Word Cloud'):
-                wordcloud = WordCloud().generate(text_data)
-
-                # Plot Word Cloud
-                plt.figure(figsize=(10, 6))
-                plt.imshow(wordcloud, interpolation='bilinear')
-                plt.axis('off')
-                st.pyplot(plt)
+        # Plot Word Cloud
+        plt.figure(figsize=(10, 6))
+        plt.imshow(wordcloud, interpolation='bilinear')
+        plt.axis('off')
+        st.pyplot(plt)
+    else:
+        st.warning("Please enter some text to generate a Word Cloud.")
