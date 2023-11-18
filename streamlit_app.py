ff88e49ae910222b750f4f27588465022ab54c9b
Akmalia Fatimah <101547800+akmalia3@users.noreply.github.com>
2023-11-18 22:14:15 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,9 +7,20 @@ st.subheader('Wordcloud Generator')
 st.write('Enjoy the app 😊')
 
 uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
-for uploaded_file in uploaded_files:
-    bytes_data = uploaded_file.read()
-    df = pd.DataFrame(bytes_data)
-    st.write("filename:", uploaded_file.name)
-    st.write(bytes_data)
-    st.dataframe(df)
+
+if uploaded_files:
+    for uploaded_file in uploaded_files:
+        df = pd.read_csv(uploaded_file)
+        st.write("Filename:", uploaded_file.name)
+        st.dataframe(df)
+
+        # Generate Word Cloud
+        if st.button('Generate Word Cloud'):
+            text_data = ' '.join(df['your_column_name'])  # Replace 'your_column_name' with the actual column containing text data
+            wordcloud = WordCloud().generate(text_data)
+
+            # Plot Word Cloud
+            plt.figure(figsize=(10, 6))
+            plt.imshow(wordcloud, interpolation='bilinear')
+            plt.axis('off')
+            st.pyplot(plt)
