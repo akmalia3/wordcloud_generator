0ea471ecbfe919859af0e2c4cd8b1182f5d102ff
Akmalia Fatimah <101547800+akmalia3@users.noreply.github.com>
2023-11-18 22:06:07 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,10 @@ from wordcloud import WordCloud
 
 st.subheader('Wordcloud Generator')
 st.write('Enjoy the app 😊')
+
+uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
+for uploaded_file in uploaded_files:
+    bytes_data = uploaded_file.read()
+    st.write("filename:", uploaded_file.name)
+    st.write(bytes_data)
+    st.dataframe(uploaded_file)
