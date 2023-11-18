5dbd54563f6ed0366d0a450499d05bad54f943bb
Akmalia Fatimah <101547800+akmalia3@users.noreply.github.com>
2023-11-18 22:36:30 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,10 +11,10 @@ user_input = st.text_area("Enter your text here:", "")
 # Generate Word Cloud
 if st.button('Generate Word Cloud'):
     if user_input:
-        wordcloud = WordCloud().generate(user_input)
+        wordcloud = WordCloud(width = 2000, height = 1334).generate(user_input)
 
         # Plot Word Cloud
-        plt.figure(figsize=(10, 6))
+        plt.figure(figsize=(10, 10))
         plt.imshow(wordcloud, interpolation='bilinear')
         plt.axis('off')
         st.pyplot(plt)
